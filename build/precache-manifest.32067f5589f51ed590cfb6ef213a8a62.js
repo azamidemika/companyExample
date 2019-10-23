39908e5805741a7fab45a24032b2dabd4ba6f1a2
azamidemika <josuebeltran_2010@hotmail.com>
2019-10-23 14:52:53 -0600

    Production build

---
+++ build/precache-manifest.32067f5589f51ed590cfb6ef213a8a62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9783325b0bffba2bcdc578a5e29a279",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7ed91dabb5f107927a9",
+    "url": "/static/css/2.519346e7.chunk.css"
+  },
+  {
+    "revision": "631d60f15cf639ec2644",
+    "url": "/static/css/main.3440a4d5.chunk.css"
+  },
+  {
+    "revision": "d7ed91dabb5f107927a9",
+    "url": "/static/js/2.1474eda7.chunk.js"
+  },
+  {
+    "revision": "631d60f15cf639ec2644",
+    "url": "/static/js/main.c6418c24.chunk.js"
+  },
+  {
+    "revision": "6acef4a274089667cbc7",
+    "url": "/static/js/runtime-main.6dba7cfb.js"
+  }
+]);
